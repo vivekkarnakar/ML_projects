7f5621a35826663de3cb3818e18444ee3a123fb1
vivekkarnakar <karnakarvivek@gmail.com>
2025-06-03 22:06:32 +0530

    Model Trainer

---
+++ src/utils.py
@@ -0,0 +1,41 @@
+import os, sys
+import numpy as np
+import pandas as pd
+import dill
+from sklearn.metrics import r2_score
+
+from src.exception import CustomException
+
+def save_object(file_path, obj):
+    try:
+        dir_path = os.path.dirname(file_path)
+
+        os.makedirs(dir_path, exist_ok=True)
+
+        with open(file_path, "wb") as file_obj:
+            dill.dump(obj, file_obj)
+
+    except Exception as e:
+        raise CustomException(e, sys)
+    
+def evaluate_models(x_train, y_train, x_test, y_test, models):
+    try:
+        report = {}
+
+        for i in range(len(list(models))):
+            model = list(models.values())[i]
+
+            model.fit(x_train, y_train)
+
+            y_train_pred = model.predict(x_train)
+            y_test_pred = model.predict(x_test)
+
+            train_model_score = r2_score(y_train, y_train_pred)
+            test_model_Score = r2_score(y_test, y_test_pred)
+
+            report[list(models.keys())[i]] = test_model_Score
+
+        return report
+    
+    except Exception as e:
+        raise CustomException(e, sys)
