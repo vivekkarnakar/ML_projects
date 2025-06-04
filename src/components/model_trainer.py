1c73bc278e5590d44055dbfcdecfc28304dcb41d
vivekkarnakar <karnakarvivek@gmail.com>
2025-06-05 00:44:09 +0530

    _

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -41,10 +41,10 @@ class modelTrainer:
                 "Decision Tree": DecisionTreeRegressor(),
                 "Gradient Boosting": GradientBoostingRegressor(),
                 "Linear Regression": LinearRegression(),
-                "K-Neighbors Classifier": KNeighborsRegressor(),
-                "XGBClassifier": XGBRegressor(),
-                "CatBoosting Classifier": CatBoostRegressor(verbose=False),
-                "Adaboost Classifier": AdaBoostRegressor()
+                "K-Neighbors Regressor": KNeighborsRegressor(),
+                "XGBRegressor": XGBRegressor(),
+                "CatBoosting Regressor": CatBoostRegressor(verbose=False),
+                "AdaBoost Regressor": AdaBoostRegressor()
             }
 
             params = {
@@ -53,36 +53,36 @@ class modelTrainer:
                     # "splitter": ["best", "random"],
                     # "max_features": ["sqrt", "log2"]
                 },
-                "Rnadom Forest": {
+                "Random Forest": {
                     # "criterion": ["squared_error", "friedman_mse", "absolute_error", "poisson"],
                     # "max_features": ["sqrt", "log2", None],
                     "n_estimators": [8, 16, 32, 64, 128, 256]
                 },
                 "Gradient Boosting": {
                     # "loss": ["squared_error", "huber", "absolute_error", "quantile"],
-                    "learning_rate": [.1, .01, .05, .001],
+                    "learning_rate": [0.1, 0.01, 0.05, 0.001],
                     "subsample": [0.6, 0.7, 0.75, 0.8, 0.85, 0.9],
                     # "criterion": ["squared_error", "friedman_mse"],
                     # "max_features": ["auto", "sqrt", "log2"],
                     "n_estimators": [8, 16, 32, 64, 128, 256]
                 },
                 "Linear Regression": {},
-                "K-Neighbour Regressor": {
+                "K-Neighbors Regressor": {
                     "n_neighbors": [5, 7, 9, 11],
                     # "weights": ["uniform", "distance"],
                     # "algorithm": ["ball_tree", "kd_tree", "brute"]
                 },
                 "XGBRegressor": {
-                    "learning_rate": [.1, .01, .05, .001],
-                    "m_estimators": [8, 16, 32, 64, 128, 256]
+                    "learning_rate": [0.1, 0.01, 0.05, 0.001],
+                    "n_estimators": [8, 16, 32, 64, 128, 256]
                 },
                 "CatBoosting Regressor": {
                     "depth": [6, 8, 10],
                     "learning_rate": [0.01, 0.05, 0.1],
-                    "iteration": [30, 50, 100]
+                    "iterations": [30, 50, 100]
                 },
                 "AdaBoost Regressor": {
-                    "learning_rate": [.1, .01, 0.5, .001],
+                    "learning_rate": [0.1, 0.01, 0.5, 0.001],
                     # "loss": ["linear", "square", "exponential"],
                     "n_estimators": [8, 16, 32, 64, 128, 256] 
                 }
